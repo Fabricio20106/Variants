6eb6aae7f57d67517ab5dbc6b80b1efa638d702e
Fabricio20106 <85804854+Fabricio20106@users.noreply.github.com>
2021-06-17 13:55:58 -0300

    some new craftings gold cauldron 3d item gold beacon with glass case is no more!

--- src/main/java/com/fabricio/variants/init/StuffInit.java
+++ src/main/java/com/fabricio/variants/init/StuffInit.java
@@ -2,6 +2,7 @@ package com.fabricio.variants.init;
 
 import com.fabricio.variants.Variants;
 import com.fabricio.variants.items.HoneyBallItem;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.item.minecart.AbstractMinecartEntity;
 import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
@@ -13,8 +14,10 @@ public class StuffInit {
 
     public static final RegistryObject<Item> gold_cauldron = items.register("gold_cauldron", () -> new BlockItem(BlockInit.gold_cauldron.get(), new Item.Properties().rarity(Rarity.UNCOMMON).tab(VariantsTab.variant)));
     public static final RegistryObject<Item> gold_beacon = items.register("gold_beacon", () -> new BlockItem(BlockInit.gold_beacon.get(), new Item.Properties().rarity(Rarity.UNCOMMON).tab(VariantsTab.variant)));
+    public static final RegistryObject<Item> golden_carrots = items.register("golden_carrots", () -> new BlockItem(BlockInit.golden_carrots.get(), new Item.Properties().rarity(Rarity.UNCOMMON).tab(VariantsTab.variant)));
+    public static final RegistryObject<Item> potted_sugar_cane = items.register("potted_sugar_cane", () -> new BlockItem(BlockInit.potted_sugar_cane.get(), new Item.Properties().rarity(Rarity.UNCOMMON).tab(VariantsTab.variant)));
     public static final RegistryObject<Item> cyan_shulker_shell = items.register("cyan_shulker_shell", () -> new Item(new Item.Properties().tab(VariantsTab.variant)));
-    public static final RegistryObject<Item> spawner_minecart = items.register("spawner_minecart", () -> new MinecartItem(AbstractMinecartEntity.Type.SPAWNER, new Item.Properties().tab(VariantsTab.variant)));
+    public static final RegistryObject<Item> spawner_minecart = items.register("spawner_minecart", () -> new MinecartItem(AbstractMinecartEntity.Type.SPAWNER, new Item.Properties().rarity(Rarity.UNCOMMON).stacksTo(1).tab(VariantsTab.variant)));
     public static final RegistryObject<Item> fungi_stew = items.register("fungi_stew", () -> new SoupItem(new Item.Properties().tab(VariantsTab.variant).food(Foods.MUSHROOM_STEW)));
     public static final RegistryObject<Item> splash_experience_bottle = items.register("splash_experience_bottle", () -> new ExperienceBottleItem(new Item.Properties().rarity(Rarity.UNCOMMON).tab(VariantsTab.variant)));
     public static final RegistryObject<Item> lingering_experience_bottle = items.register("lingering_experience_bottle", () -> new ExperienceBottleItem(new Item.Properties().rarity(Rarity.UNCOMMON).tab(VariantsTab.variant)));
