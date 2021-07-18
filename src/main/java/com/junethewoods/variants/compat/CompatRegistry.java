b3462b8d2a9a407d407db01777fff86b1cae3a03
Fabricio20106 <85804854+Fabricio20106@users.noreply.github.com>
2021-07-18 13:32:32 -0300

    added compat w/ creeperedits

--- src/main/java/com/junethewoods/variants/compat/CompatRegistry.java
+++ src/main/java/com/junethewoods/variants/compat/CompatRegistry.java
@@ -8,11 +8,12 @@ public class CompatRegistry {
     public static void registerF10Compats() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         final ModList list = ModList.get();
-        if (list.isLoaded("fabricio2001pack")) F10CompatBlocks.fabricio2010packCompat.register(modEventBus);
+        if (list.isLoaded("fabricio2010pack")) F10CompatBlocks.fabricio2010packCompat.register(modEventBus);
         if (list.isLoaded("fabricio2010pack")) F10CompatItems.fabricio2010packCompat.register(modEventBus);
         if (list.isLoaded("backmath")) F10CompatItems.backMathCompat.register(modEventBus);
         if (list.isLoaded("edits")) F10CompatItems.editsCompat.register(modEventBus);
         if (list.isLoaded("others")) F10CompatItems.othersCompat.register(modEventBus);
+        if (list.isLoaded("creeperedits")) F10CompatItems.creeperEditsCompat.register(modEventBus);
     }
 
     public static void registerCompats() {
